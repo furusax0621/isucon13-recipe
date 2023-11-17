a7ce36d67f9577329fb206c633020c99f7f35317
Tatsuro Furusawa <fur.sax.smallworld@gmail.com>
2023-11-17 16:17:31 +0900

    バージョンミスってた

--- cookbooks/sml/default.rb
+++ cookbooks/sml/default.rb
@@ -2,5 +2,5 @@
 github_binary 'sml' do
   repository 'furusax0621/slack-messenger-lite'
   version 'v0.0.4'
-  archive 'sml_0.0.2_Linux_x86_64.tar.gz'
+  archive 'sml_0.0.4_Linux_x86_64.tar.gz'
 end
