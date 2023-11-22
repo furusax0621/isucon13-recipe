f7760e28f5bdbd5adecd90ffb72852b2a2743e38
Tatsuro Furusawa <fur.sax.smallworld@gmail.com>
2023-11-22 10:45:23 +0900

    update sml

--- cookbooks/sml/default.rb
+++ cookbooks/sml/default.rb
@@ -1,6 +1,6 @@
 # https://github.com/furusax0621/slack-messenger-lite
 github_binary 'sml' do
   repository 'furusax0621/slack-messenger-lite'
-  version 'v0.0.4'
-  archive 'sml_0.0.4_Linux_x86_64.tar.gz'
+  version 'v0.0.6'
+  archive 'sml_0.0.6_Linux_x86_64.tar.gz'
 end
